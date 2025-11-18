2d6944dc58bbe038196c4569bc8666be8af05c60
Saadelquaul <Saadelquaul@gmail.com>
2025-11-18 11:45:48 +0100

    initial commit

---
+++ src/main/java/com/logistique/colis_system/ColisSystemApplication.java
@@ -0,0 +1,13 @@
+package com.logistique.colis_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColisSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColisSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistique/colis_system/ColisSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistique.colis_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColisSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
