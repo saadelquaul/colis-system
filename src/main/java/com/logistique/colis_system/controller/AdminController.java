2bc48c8e0abed38960bb820ff732a376bfa6277c
Saadelquaul <Saadelquaul@gmail.com>
2025-11-21 17:13:32 +0100

    rename AdminTransporteurController to AdminController for improved clarity

--- src/main/java/com/logistique/colis_system/controller/AdminController.java
+++ src/main/java/com/logistique/colis_system/controller/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
-public class AdminTransporteurController {
+public class AdminController {
 
     @Autowired
     private TransporteurService transporteurService;
