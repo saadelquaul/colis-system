6ca867c299046708e9890b1e51769fc6100ae8f7
Saadelquaul <Saadelquaul@gmail.com>
2025-11-21 16:48:36 +0100

    add pagination methods for retrieving transporteurs by role and specialite in UserRepository

--- src/main/java/com/logistique/colis_system/repository/UserRepository.java
+++ src/main/java/com/logistique/colis_system/repository/UserRepository.java
@@ -4,6 +4,8 @@ import com.logistique.colis_system.model.Transporteur;
 import com.logistique.colis_system.model.User;
 import com.logistique.colis_system.model.enums.Role;
 import com.logistique.colis_system.model.enums.Specialite;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,7 +20,13 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByLogin(String login);
     List<User> findByRole(Role role);
 
+
+    Page<User> findByActiveTrue(Pageable pageable);
+    
     @Query("{ 'role': 'TRANSPORTEUR', 'specialite': ?0 }")
     List<Transporteur> findTransporteursBySpecialite(Specialite specialite);
+
+    Page<Transporteur> findByRole(Role role, Pageable pageable);
+    Page<Transporteur> findByRoleAndSpecialite(Role role, Specialite specialite, Pageable pageable);
 
 }
