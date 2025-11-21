fbc354348fa93229983852daa6e9ed25ed2013e7
Saadelquaul <Saadelquaul@gmail.com>
2025-11-21 12:12:31 +0100

    add CustomUserDetailsService for user authentication and role management

---
+++ src/main/java/com/logistique/colis_system/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.logistique.colis_system.security;
+
+import com.logistique.colis_system.model.User;
+import com.logistique.colis_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with login: " + login));
+
+        // 2. Check if account is active
+        if (!user.isActive()) {
+            throw new UsernameNotFoundException("User account is deactivated");
+        }
+
+        // 3. Convert our Mongo "User" to Spring Security's "UserDetails"
+        // We add "ROLE_" prefix because Spring Security expects it by default
+        return new org.springframework.security.core.userdetails.User(
+                user.getLogin(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
