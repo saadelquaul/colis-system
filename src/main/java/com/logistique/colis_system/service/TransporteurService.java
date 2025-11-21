60ea8385c06a5dc1da5df5b1f0b261158e2626e1
Saadelquaul <Saadelquaul@gmail.com>
2025-11-21 16:06:23 +0100

    add TransporteurService for managing transporteur creation and validation

---
+++ src/main/java/com/logistique/colis_system/service/TransporteurService.java
@@ -0,0 +1,36 @@
+package com.logistique.colis_system.service;
+
+
+import com.logistique.colis_system.dto.TransporteurDTO;
+import com.logistique.colis_system.model.Transporteur;
+import com.logistique.colis_system.model.enums.TransporteurStatut;
+import com.logistique.colis_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransporteurService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public Transporteur createTransporteur(TransporteurDTO transporteurDTO) {
+        if (userRepository.findByLogin(transporteurDTO.getLogin()).isPresent()) {
+            throw new RuntimeException("Login already exists");
+        }
+
+        Transporteur transporteur = new Transporteur();
+        transporteur.setLogin(transporteurDTO.getLogin());
+        transporteur.setPassword(passwordEncoder.encode(transporteurDTO.getPassword()));
+        transporteur.setSpecialite(transporteurDTO.getSpecialite());
+        transporteur.setStatut(TransporteurStatut.DISPONIBLE);
+        transporteur.setActive(true);
+
+        return userRepository.save(transporteur);
+    }
+}
