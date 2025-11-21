476e332d1a93337150079e80daa6afeb5a426e52
Saadelquaul <Saadelquaul@gmail.com>
2025-11-21 16:06:05 +0100

    add @Component annotation to DataSeeder for Spring component scanning

--- src/main/java/com/logistique/colis_system/config/DataSeeder.java
+++ src/main/java/com/logistique/colis_system/config/DataSeeder.java
@@ -6,7 +6,10 @@ import com.logistique.colis_system.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class DataSeeder implements CommandLineRunner {
 
     @Autowired
