ec142483ea2cb61e39105bedf03f72ae78f92526
Saadelquaul <Saadelquaul@gmail.com>
2025-11-21 14:58:24 +0100

    add DataSeeder for initializing admin user in the database

---
+++ src/main/java/com/logistique/colis_system/config/DataSeeder.java
@@ -0,0 +1,30 @@
+package com.logistique.colis_system.config;
+
+import com.logistique.colis_system.model.Admin;
+import com.logistique.colis_system.model.enums.Role;
+import com.logistique.colis_system.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class DataSeeder implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if(userRepository.findByRole(Role.ADMIN).isEmpty()) {
+            Admin admin = new Admin();
+            admin.setLogin("admin");
+
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setActive(true);
+
+            userRepository.save(admin);
+        }
+    }
+}
