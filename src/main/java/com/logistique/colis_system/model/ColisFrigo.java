02e8239b3ad9f8155f83e2a289ea435e6c66a4b2
Saadelquaul <Saadelquaul@gmail.com>
2025-12-21 13:11:29 +0100

    add Lombok annotations to ColisFrigo class for boilerplate reduction

--- src/main/java/com/logistique/colis_system/model/ColisFrigo.java
+++ src/main/java/com/logistique/colis_system/model/ColisFrigo.java
@@ -2,9 +2,11 @@ package com.logistique.colis_system.model;
 
 
 import com.logistique.colis_system.model.enums.Specialite;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
+@Data
 public class ColisFrigo extends Colis{
 
     private Double temperatureMin;
